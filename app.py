630dcb501ad5d7522647a8f91cedbb3ee55cbddf
danmjp19-boop <danmjp19@gmail.com>
2026-07-17 06:26:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def inicio():
-    return "<h1>Sistema de Control de Personal Excusado</h1><p>Aplicación funcionando correctamente.</p>"
+    return render_template("login.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
