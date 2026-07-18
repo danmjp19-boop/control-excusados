8f22d6165a9928f798a1f4176ea1e572b2d46798
danmjp19-boop <danmjp19@gmail.com>
2026-07-17 23:38:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -66,6 +66,10 @@ def login():
 
     return render_template("login.html")
 
+@app.route("/usuarios")
+def usuarios():
+    return render_template("usuarios.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
