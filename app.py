22792ada167af80e5bf9b389328cd6a1c2366fab
danmjp19-boop <danmjp19@gmail.com>
2026-07-17 21:03:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.secret_key = "control_excusados_2026"
 
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -12,14 +13,16 @@ db = SQLAlchemy(app)
 class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     usuario = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(200), nullable=False)
-
-@app.route("/")
-def inicio():
-    return render_template("login.html")
+    password = db.Column(db.String(100), nullable=False)
+    rol = db.Column(db.String(20), nullable=False)
+    cai = db.Column(db.String(50), nullable=False)
 
 with app.app_context():
     db.create_all()
+
+@app.route("/", methods=["GET","POST"])
+def login():
+    return render_template("login.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
