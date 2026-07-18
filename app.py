7146080d9f5fd27d9355b3aeaac8cd235d773822
danmjp19-boop <danmjp19@gmail.com>
2026-07-17 21:14:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,29 +3,13 @@ from flask import Flask, render_template, request, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
-from flask_sqlalchemy import SQLAlchemy
-import os
+app.secret_key = "control_excusados_2026"
 
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
-class Usuario(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    usuario = db.Column(db.String(50), unique=True)
-    password = db.Column(db.String(100))
-    rol = db.Column(db.String(50))
-    cai = db.Column(db.String(50))
-
-
-app.secret_key = "control_excusados_2026"
-
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-db = SQLAlchemy(app)
 
 class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -34,12 +18,6 @@ class Usuario(db.Model):
     rol = db.Column(db.String(20), nullable=False)
     cai = db.Column(db.String(50), nullable=False)
 
-with app.app_context():
-    db.create_all()
-
-@app.route("/", methods=["GET","POST"])
-def login():
-    return render_template("login.html")
 
 with app.app_context():
     db.create_all()
@@ -53,7 +31,12 @@ with app.app_context():
         )
         db.session.add(admin)
         db.session.commit()
-        
+
+
+@app.route("/", methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
