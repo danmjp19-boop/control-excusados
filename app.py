a55fecbfcb8f632fc7389d7340b32248d99fe945
danmjp19-boop <danmjp19@gmail.com>
2026-07-17 20:39:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
+import os
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    usuario = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
 
 @app.route("/")
 def inicio():
     return render_template("login.html")
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
     app.run(debug=True)
